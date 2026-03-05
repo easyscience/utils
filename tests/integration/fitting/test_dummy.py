ef160ae16d40efc066bef1a55dee1c9f41a7f462
Andrew Sazonov <andrew.sazonov@icloud.com>
2026-03-05 09:08:52 +0100

    Update spdx headers

--- tests/integration/fitting/test_dummy.py
+++ tests/integration/fitting/test_dummy.py
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: 2026 EasyUtilities contributors <https://github.com/easyscience>
+# SPDX-FileCopyrightText: 2026 EasyScience contributors <https://github.com/easyscience>
 # SPDX-License-Identifier: BSD-3-Clause
 
 import pytest
